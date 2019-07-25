4857fbd20950c9cd749abe76e8a9c29636e931ba
Renan Rodrigo <renanrb@ime.usp.br>
2019-07-25 20:58:38 -0300

    Implementar logica de vencer o jogo

--- source/js/script.js
+++ source/js/script.js
@@ -14,5 +14,35 @@ for(let quadradinho of quadradinhos){
     controleClique = !controleClique;
 
     quadradinho.onclick = null;
+
+    if((quadradinhos[0].style.backgroundImage === quadradinhos[1].style.backgroundImage 
+      && quadradinhos[1].style.backgroundImage === quadradinhos[2].style.backgroundImage 
+      && quadradinhos[0].style.backgroundImage !== "")
+      || (quadradinhos[3].style.backgroundImage === quadradinhos[4].style.backgroundImage 
+        && quadradinhos[4].style.backgroundImage === quadradinhos[5].style.backgroundImage 
+        && quadradinhos[3].style.backgroundImage !== "")
+        ||(quadradinhos[6].style.backgroundImage === quadradinhos[7].style.backgroundImage 
+          && quadradinhos[7].style.backgroundImage === quadradinhos[8].style.backgroundImage 
+          && quadradinhos[6].style.backgroundImage !== "")
+          || (quadradinhos[0].style.backgroundImage === quadradinhos[3].style.backgroundImage 
+            && quadradinhos[3].style.backgroundImage === quadradinhos[6].style.backgroundImage 
+            && quadradinhos[0].style.backgroundImage !== "")
+            ||(quadradinhos[1].style.backgroundImage === quadradinhos[4].style.backgroundImage 
+              && quadradinhos[4].style.backgroundImage === quadradinhos[7].style.backgroundImage 
+              && quadradinhos[1].style.backgroundImage !== "")
+              ||(quadradinhos[2].style.backgroundImage === quadradinhos[5].style.backgroundImage 
+                && quadradinhos[5].style.backgroundImage === quadradinhos[8].style.backgroundImage 
+                && quadradinhos[2].style.backgroundImage !== "")
+                ||(quadradinhos[0].style.backgroundImage === quadradinhos[4].style.backgroundImage 
+                  && quadradinhos[4].style.backgroundImage === quadradinhos[8].style.backgroundImage 
+                  && quadradinhos[0].style.backgroundImage !== "")
+                  ||(quadradinhos[2].style.backgroundImage === quadradinhos[4].style.backgroundImage 
+                    && quadradinhos[4].style.backgroundImage === quadradinhos[6].style.backgroundImage 
+                    && quadradinhos[2].style.backgroundImage !== "")){
+      for(let cada of quadradinhos){
+        console.log("fim de jogo");
+        cada.onclick = null;
+      }
+    }
   }
 }
