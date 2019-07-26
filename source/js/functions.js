27409323d89c1a4607e0bde08de39b0eee23265e
Renan Rodrigo <renanrb@ime.usp.br>
2019-07-25 21:38:49 -0300

    Refatorar o codigo com funcoes

---
+++ source/js/functions.js
@@ -0,0 +1,22 @@
+function iguais(indice1, indice2, indice3){
+  if(quadradinhos[indice1].style.backgroundImage === quadradinhos[indice2].style.backgroundImage
+    && quadradinhos[indice2].style.backgroundImage === quadradinhos[indice3].style.backgroundImage
+    && quadradinhos[indice1].style.backgroundImage !== ""){
+      return true;
+    }
+  return false;
+}
+
+function alguemGanhou() {
+  if (iguais(0, 1, 2) || iguais(3, 4, 5) || iguais(6, 7, 8)
+    || iguais(0, 3, 6) || iguais(1, 4, 7) || iguais(2, 5, 8)
+    || iguais(0, 4, 8)  || iguais(2, 4, 6)) {
+    return true;
+  }
+  return false;
+}
+
+function escolherQuadradinho(quadradinho, imagem){
+  quadradinho.style.backgroundImage = `url("imagens/${imagem}.jpg")`;
+};
+
--- source/js/script.js
+++ source/js/script.js
@@ -4,43 +4,23 @@ let controleClique = true;
 
 for(let quadradinho of quadradinhos){
   quadradinho.onclick = function(){
+    let simbolo;
     if(controleClique){
-      quadradinho.style.backgroundImage = 'url("imagens/X.jpg")';
+      simbolo = "X";
     }
     else{
-      quadradinho.style.backgroundImage = 'url("imagens/O.jpg")';
+      simbolo = "O";
     }
+
+    escolherQuadradinho(quadradinho, simbolo);
 
     controleClique = !controleClique;
 
     quadradinho.onclick = null;
 
-    if((quadradinhos[0].style.backgroundImage === quadradinhos[1].style.backgroundImage 
-      && quadradinhos[1].style.backgroundImage === quadradinhos[2].style.backgroundImage 
-      && quadradinhos[0].style.backgroundImage !== "")
-      || (quadradinhos[3].style.backgroundImage === quadradinhos[4].style.backgroundImage 
-        && quadradinhos[4].style.backgroundImage === quadradinhos[5].style.backgroundImage 
-        && quadradinhos[3].style.backgroundImage !== "")
-        ||(quadradinhos[6].style.backgroundImage === quadradinhos[7].style.backgroundImage 
-          && quadradinhos[7].style.backgroundImage === quadradinhos[8].style.backgroundImage 
-          && quadradinhos[6].style.backgroundImage !== "")
-          || (quadradinhos[0].style.backgroundImage === quadradinhos[3].style.backgroundImage 
-            && quadradinhos[3].style.backgroundImage === quadradinhos[6].style.backgroundImage 
-            && quadradinhos[0].style.backgroundImage !== "")
-            ||(quadradinhos[1].style.backgroundImage === quadradinhos[4].style.backgroundImage 
-              && quadradinhos[4].style.backgroundImage === quadradinhos[7].style.backgroundImage 
-              && quadradinhos[1].style.backgroundImage !== "")
-              ||(quadradinhos[2].style.backgroundImage === quadradinhos[5].style.backgroundImage 
-                && quadradinhos[5].style.backgroundImage === quadradinhos[8].style.backgroundImage 
-                && quadradinhos[2].style.backgroundImage !== "")
-                ||(quadradinhos[0].style.backgroundImage === quadradinhos[4].style.backgroundImage 
-                  && quadradinhos[4].style.backgroundImage === quadradinhos[8].style.backgroundImage 
-                  && quadradinhos[0].style.backgroundImage !== "")
-                  ||(quadradinhos[2].style.backgroundImage === quadradinhos[4].style.backgroundImage 
-                    && quadradinhos[4].style.backgroundImage === quadradinhos[6].style.backgroundImage 
-                    && quadradinhos[2].style.backgroundImage !== "")){
+    if(alguemGanhou()){
+      console.log("fim de jogo");
       for(let cada of quadradinhos){
-        console.log("fim de jogo");
         cada.onclick = null;
       }
     }
